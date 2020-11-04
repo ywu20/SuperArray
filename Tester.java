e47e7d5332c34235116ce5c3b1d3f3dc8fade814
Yuqing Wu <ywu20@stuy.edu>
2020-11-04 13:23:48 -0500

    basic tests for everything

--- Tester.java
+++ Tester.java
@@ -1,6 +1,6 @@
+import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    /*
   SuperArray words = new SuperArray();
   System.out.println(words.size());
   words.add("kani");
@@ -23,6 +23,23 @@ word.add("ebi");
 for(int i = 0; i < word.size(); i++){
   System.out.println( word.get(i) );
 }
-*/
+
+SuperArray a = new SuperArray();
+System.out.println(a.isEmpty());
+System.out.println(word.isEmpty());
+word.clear();
+System.out.println(word);
+System.out.println(words);
+System.out.println(words.contains("add"));
+System.out.println(words.contains("ADD"));
+words.add(1,"hello");
+System.out.println(words);
+System.out.println(words.remove(3));
+System.out.println(words);
+System.out.println(words.indexOf("add"));
+System.out.println(words.indexOf("ADD"));
+System.out.println(Arrays.toString(words.toArray()));
+
+
   }
 }
