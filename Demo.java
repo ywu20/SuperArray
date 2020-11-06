044020e875b2314e2f3206ffdc0c5e7dc15c3c84
Yuqing Wu <ywu20@stuy.edu>
2020-11-06 13:42:04 -0500

    zip

--- Demo.java
+++ Demo.java
@@ -18,9 +18,30 @@ public class Demo{
     }
     return out;
   }
+
+public static SuperArray zipHelper(SuperArray small, SuperArray big, SuperArray a, SuperArray b){
+  SuperArray out = new SuperArray();
+  for(int i=0; i<big.size();i++){
+    if(i<small.size()){
+      out.add(a.get(i));
+      out.add(b.get(i));
+    }
+    else{
+      out.add(big.get(i));
+    }
+  }
+  return out;
+}
+  public static SuperArray zip(SuperArray a, SuperArray b){
+    if(a.size()>b.size()){
+      return zipHelper(b,a,a,b);
+    }
+    return zipHelper(a,b,a,b);
+  }
+
   public static void main(String[] args){
     SuperArray words = new SuperArray();
-
+/*
   //grouped to save vertical space
   words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
   words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
@@ -29,7 +50,7 @@ public class Demo{
   System.out.println(words);
   removeDuplicates(words);
   System.out.println(words);
-
+*/
 
 SuperArray a = new SuperArray();
 SuperArray b = new SuperArray();
@@ -41,7 +62,18 @@ SuperArray b = new SuperArray();
 
   System.out.println(a);
   System.out.println(b);
-  System.out.println(findOverlap(a,b));
+//  System.out.println(findOverlap(a,b));
+//  System.out.println(a.lastIndexOf("una"));
+
+
+  SuperArray c = new SuperArray();
+  SuperArray d= new SuperArray(4);
+  c.add("kani");   c.add("uni");     c.add("ebi");     c.add("una");
+  d.add("kani");    d.add("uni");    d.add("ebi");    d.add("una");
+//System.out.println(c.equals(d));
+//System.out.println(a.equals(b));
+
+System.out.println(zip(a,b));
 
   }
 }
